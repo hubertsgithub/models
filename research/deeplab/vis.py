24b271bb829e7d941cc89bd46e5ad74e4f445c1f
hubertsgithub <1hubertlin@gmail.com>
2018-05-20 14:56:00 -0400

    hints flag vis

--- research/deeplab/vis.py
+++ research/deeplab/vis.py
@@ -70,6 +70,11 @@ flags.DEFINE_multi_float('eval_scales', [1.0],
 flags.DEFINE_bool('add_flipped_images', False,
                   'Add flipped images for evaluation or not.')
 
+# hints=True for image inputs with additional channel for hints
+# TODO incorporate this into ModelOptions
+flags.DEFINE_bool('hints', False,
+                  'Input has additional hint channel or not.')
+
 # Dataset settings.
 
 flags.DEFINE_string('dataset', 'pascal_voc_seg',
@@ -222,16 +227,22 @@ def main(unused_argv):
         atrous_rates=FLAGS.atrous_rates,
         output_stride=FLAGS.output_stride)
 
+    if FLAGS.hints:
+        model_inputs = tf.concat([samples[common.IMAGE], tf.to_float(samples[common.HINT])], axis=-1)
+        print '***DEBUG common.HINT is currently set to LABEL in eval.py TODO'
+    else:
+        model_inputs = samples[common.IMAGE]
+
     if tuple(FLAGS.eval_scales) == (1.0,):
       tf.logging.info('Performing single-scale test.')
       predictions = model.predict_labels(
-          samples[common.IMAGE],
+          model_inputs,
           model_options=model_options,
           image_pyramid=FLAGS.image_pyramid)
     else:
       tf.logging.info('Performing multi-scale test.')
       predictions = model.predict_labels_multi_scale(
-          samples[common.IMAGE],
+          model_inputs,
           model_options=model_options,
           eval_scales=FLAGS.eval_scales,
           add_flipped_images=FLAGS.add_flipped_images)
